0c2086e300c9c33c83386b7877f48a7198d13614
Rachel Lacobelle <44063772+rlacobelle20@users.noreply.github.com>
2021-07-21 02:42:06 -0400

    completed output_steps

--- mario.cpp
+++ mario.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <string>
 
-unsigned int steps = 1;
-std::cin >> steps >> "How many steps? ";
+int main() {
+    unsigned int steps = 1;
+    std::cin >> steps >> "How many steps? ";
+
+    output_steps(steps);
+
+    return 0;
+}
+
+void output_steps(unsigned int steps) {
+    for (unsigned int i = 0; i < steps; i++) {
+        // must print step - i amount of spaces
+        // print i amount of #
+
+        std::string spaces(' ', steps - i);
+        std::string hashtag('#', i);
+
+        std::cout << spaces << hashtag << std::endl;
+    }
+}
